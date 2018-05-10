6b86bb2e35a77829d7f97846cf8f11cf050d9172
Alex Dean <alex@snowplowanalytics.com>
2018-05-10 12:07:33 +0100

    Add Bintray credentials to .travis.yml (close #9)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -15,9 +15,7 @@ cd $TRAVIS_BUILD_DIR
 
 project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /(\d+\.\d+\.\d+[^\r\n]*)/' | head -n 1 | tr -d '\n')
 if [ "${project_version}" == "${tag_version}" ]; then
-    sbt protocols/publish
     sbt client/publish
-    sbt protocols/bintraySyncMavenCentral
     sbt client/bintraySyncMavenCentral
 else
     echo "Tag version '${tag_version}' doesn't match version in scala project ('${project_version}'). Aborting!"
